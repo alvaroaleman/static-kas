831d1ed2eedcdf948ff8f3b93897eb4835518510
Raphaël Pinson <raphael@isovalent.com>
2022-03-16 15:47:25 +0100

    Add support for label selectors (#11)
    * feat: add label filter
    
    Signed-off-by: Raphaël Pinson <raphael@isovalent.com>
    
    * Update pkg/filter/filter.go
    
    Co-authored-by: Alvaro Aleman <alvaroaleman@users.noreply.github.com>
    
    Co-authored-by: Alvaro Aleman <alvaroaleman@users.noreply.github.com>

--- pkg/filter/filter.go
+++ pkg/filter/filter.go
@@ -6,13 +6,18 @@ import (
 	"strings"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
 )
 
 type Filter func(*unstructured.UnstructuredList) (*unstructured.UnstructuredList, error)
 
 func FromRequest(r *http.Request) []Filter {
-	return []Filter{filterForFieldSelector(r.URL.Query()["fieldSelector"])}
+	return []Filter{
+		filterForFieldSelector(r.URL.Query()["fieldSelector"]),
+		filterForLabels(r.URL.Query()["labelSelector"]),
+	}
 }
+
 func filterForFieldSelector(value []string) Filter {
 	selectorMap := make(map[string]string, len(value))
 	return func(in *unstructured.UnstructuredList) (*unstructured.UnstructuredList, error) {
@@ -38,6 +43,41 @@ func filterForFieldSelector(value []string) Filter {
 			matches := true
 			for k, v := range selectorMap {
 				if value, _, _ := unstructured.NestedString(item.Object, strings.Split(k, ".")...); value != v {
+					matches = false
+					break
+				}
+			}
+			if matches {
+				result.Items = append(result.Items, *item.DeepCopy())
+			}
+		}
+
+		return result, nil
+	}
+}
+
+func filterForLabels(value []string) Filter {
+	var selectors []labels.Selector
+	return func(in *unstructured.UnstructuredList) (*unstructured.UnstructuredList, error) {
+		if len(value) == 0 {
+			return in, nil
+		}
+		for _, entry := range value {
+			selector, err := labels.Parse(entry)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse label selector %s: %w", entry, err)
+			}
+			selectors = append(selectors, selector)
+		}
+
+		result := &unstructured.UnstructuredList{}
+		result.SetGroupVersionKind(in.GroupVersionKind())
+
+		for _, item := range in.Items {
+			matches := true
+			labels := labels.Set(item.GetLabels())
+			for _, sel := range selectors {
+				if !sel.Matches(labels) {
 					matches = false
 					break
 				}
