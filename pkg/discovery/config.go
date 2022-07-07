5dda75be64b0b59d2e13aa949d7cb8e5b5bdd536
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2022-07-07 11:52:56 -0400

    Add shortnames for replicaets and services (#31)

--- pkg/discovery/config.go
+++ pkg/discovery/config.go
@@ -3,8 +3,10 @@ package discovery
 // TODO: Import this somehow
 var shortNameMapping = map[string][]string{
 	"pods":              {"po"},
+	"services":          {"svc"},
 	"statefulsets.apps": {"sts"},
 	"deployments.apps":  {"dep"},
 	"daemonsets.apps":   {"ds"},
+	"replicasets.apps":  {"rs"},
 	"customresourcedefinitions.apiextensions.k8s.io": {"crd", "crds"},
 }
